49d1fdd5be12968f356b9b01d20c86304735823f
Marcelo Bueno <marcelobueno25@hotmail.com>
2023-04-06 08:37:11 -0300

    ajuste importacao

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
-import { LayoutContainer, Container } from './styles'
+import { Container } from './styles'
 
 export function DefaultLayout() {
   return (
